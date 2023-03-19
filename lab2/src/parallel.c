de264429b7504f11c3286859db07f375688fea7b
mihanizzm <m.sartakov@g.nsu.ru>
2023-03-20 01:07:47 +0700

    working parallel version

--- lab2/src/parallel.c
+++ lab2/src/parallel.c
@@ -4,18 +4,39 @@
 #include <mpi/mpi.h>
 
 #define SIZE_OF_VECTOR 5432
-#defint ZERO_PROCESS 0
+#define ZERO_PROCESS 0
 
 double const TAU = 0.00001;
 double const EPSILON = 1.0e-5;
 
-void freeMemory(double* v1, double* v2, double* v3, double* v4, double* v5, double* m1) {
-    free(v1);
-    free(v2);
-    free(v3);
-    free(v4);
-    free(v5);
-    free(m1);
+void deleteDouble(double* data) {
+    if (data) {
+        free(data);
+    }
+}
+
+void deleteInt(int* data) {
+    if (data) {
+        free(data);
+    }
+}
+
+void freeMemory(double* e1, double* e2, double* e3, double* e4, double* e5, double* e6, double* e7, double* e8, double* e9, double* e10, int* e11, int* e12, int* e13, int* e14) {
+    deleteDouble(e1);
+    deleteDouble(e2);
+    deleteDouble(e3);
+    deleteDouble(e4);
+    deleteDouble(e5);
+    deleteDouble(e6);
+    deleteDouble(e7);
+    deleteDouble(e8);
+    deleteDouble(e9);
+    deleteDouble(e10);
+
+    deleteInt(e11);
+    deleteInt(e12);
+    deleteInt(e13);
+    deleteInt(e14);
 }
 
 void setDataFirstVariant(double* matrixA, double* vectorB, double* vectorXn, double* vectorXn1, double* vectorAXn, double* vectorDiffAXnB) {
@@ -39,7 +60,7 @@ void setDataFirstVariant(double* matrixA, double* vectorB, double* vectorXn, dou
     }
 }
 
-void printVector(double* vector, int vectorSize) {
+void printVector(const double* vector, int vectorSize) {
     printf("Vector: [");
     for (int i = 0; i < vectorSize - 1; ++i) {
         printf("%.1lf, ", vector[i]);
@@ -56,8 +77,8 @@ void getDiffOfVectors(const double* v1, const double* v2, double* dstVector, dou
 void getMatrixVectorMultiplication(const double* srcMatrix, const double* srcVector, double* dstVector, int axisSize) {
     for (int i = 0; i < axisSize; ++i) {
         dstVector[i] = 0.0;
-        for (int j = 0; j < axisSize; ++j) {
-            dstVector[i] += srcMatrix[i * axisSize + j] * srcVector[j];
+        for (int j = 0; j < SIZE_OF_VECTOR; ++j) {
+            dstVector[i] += srcMatrix[i * SIZE_OF_VECTOR + j] * srcVector[j];
         }
     }
 }
@@ -85,16 +106,15 @@ int getShift(int* sizesOfParts, int currentProcess) {
 }
 
 void calculateSquaredNormOfVector(double* dstSquaredNorm, const double* srcVector, int* sizesOfPartsOfVector, int* displacementsOfPartsOfVector, int currentProcess) {
-    double partialSquaredNorm = getSquaredNorm(srcVector + displacementsOfPartsOfVector[currentProcess] * sizeof(double), sizesOfPartsOfVector[currentProcess]);
+    double partialSquaredNorm = getSquaredNorm(srcVector + displacementsOfPartsOfVector[currentProcess], sizesOfPartsOfVector[currentProcess]);
     MPI_Allreduce(&partialSquaredNorm, dstSquaredNorm, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
 }
 
 bool isSolved(const double* partOfMatrixA, const double* vectorX, const double* vectorB, double* partOfVectorAXn, double* partOfVectorDiffAXnB, double squaredNormB, double threshold, int* sizesOfPartsOfVector, int* displacementsOfPartsOfVector, int currentProcess) {
     double squaredNormAXnB = 0.0;
     double partOfNorm = 0.0;
-
-    getMatrixVectorMultiplication(partOfMatrixA, vectorX + displacementsOfPartsOfVector[currentProcess] * sizeof(double), partOfVectorAXn, sizesOfPartsOfVector[currentProcess]);
-    getDiffOfVectors(partOfVectorAXn, vectorB + displacementsOfPartsOfVector[currentProcess] * sizeof(double), partOfVectorDiffAXnB, 1.0, sizesOfPartsOfVector[currentProcess]);
+    getMatrixVectorMultiplication(partOfMatrixA, vectorX, partOfVectorAXn, sizesOfPartsOfVector[currentProcess]);
+    getDiffOfVectors(partOfVectorAXn, vectorB + displacementsOfPartsOfVector[currentProcess], partOfVectorDiffAXnB, 1.0, sizesOfPartsOfVector[currentProcess]);
     partOfNorm = getSquaredNorm(partOfVectorDiffAXnB, sizesOfPartsOfVector[currentProcess]);
     MPI_Allreduce(&partOfNorm, &squaredNormAXnB, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
 
@@ -103,7 +123,7 @@ bool isSolved(const double* partOfMatrixA, const double* vectorX, const double* 
 }
 
 void getNextX(double* vectorX, double* partOfVectorDiffAXnB, double* partOfVectorX1, double multiplier, int* sizesOfPartsOfVector, int* displacementsOfPartsOfVector, int currentProcess) {
-    getDiffOfVectors(vectorX + displacementsOfPartsOfVector[currentProcess] * sizeof(double), partOfVectorDiffAXnB, partOfVectorX1, multiplier, sizesOfPartsOfVector[currentProcess]);
+    getDiffOfVectors(vectorX + displacementsOfPartsOfVector[currentProcess], partOfVectorDiffAXnB, partOfVectorX1, multiplier, sizesOfPartsOfVector[currentProcess]);
     MPI_Allgatherv(partOfVectorX1, sizesOfPartsOfVector[currentProcess], MPI_DOUBLE, vectorX, sizesOfPartsOfVector, displacementsOfPartsOfVector, MPI_DOUBLE, MPI_COMM_WORLD);
 }
 
@@ -140,41 +160,24 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &currentProcess);
     MPI_Comm_size(MPI_COMM_WORLD, &numberOfProcesses);
 
-    double* vectorX = NULL;
-    double* vectorX1 = NULL;
-    double* vectorB = NULL;
-    double* vectorAX = NULL;
-    double* vectorDiffAXB = NULL;
-    double* matrixA = NULL;
+    double* vectorX = (double*)malloc(sizeof(double) * SIZE_OF_VECTOR);
+    double* vectorX1 = (double*)malloc(sizeof(double) * SIZE_OF_VECTOR);
+    double* vectorB = (double*)malloc(sizeof(double) * SIZE_OF_VECTOR);
+    double* vectorAX = (double*)malloc(sizeof(double) * SIZE_OF_VECTOR);
+    double* vectorDiffAXB = (double*)malloc(sizeof(double) * SIZE_OF_VECTOR);
+    double* matrixA = (double*)malloc(sizeof(double) * SIZE_OF_VECTOR * SIZE_OF_VECTOR);
 
-    int* sizesOfPartsOfVector = NULL;
-    int* sizesOfPartsOfMatrix = NULL;
-    int* displacementsOfPartsOfVector = NULL;
-    int* displacementsOfPartsOfMatrix = NULL;
-
-    double* partOfMatrixA = NULL;
-    double* partOfVectorX = NULL;
-    double* partOfVectorX1 = NULL;
-    double* partOfVectorAX = NULL;
-    double* partOfVectorDiffAXB = NULL;
+    int* sizesOfPartsOfVector = (int*)malloc(sizeof(int) * numberOfProcesses);
+    int* sizesOfPartsOfMatrix = (int*)malloc(sizeof(int) * numberOfProcesses);
+    int* displacementsOfPartsOfVector = (int*)malloc(sizeof(int) * numberOfProcesses);
+    int* displacementsOfPartsOfMatrix = (int*)malloc(sizeof(int) * numberOfProcesses);
 
     double squaredNormB = 0.0;
 
+    double start = 0.0;
+    double end = 0.0;
+
     if (currentProcess == ZERO_PROCESS) {
-        double start, end;
-
-        vectorX = (double*)malloc(sizeof(double) * SIZE_OF_VECTOR);
-        vectorX1 = (double*)malloc(sizeof(double) * SIZE_OF_VECTOR);
-        vectorB = (double*)malloc(sizeof(double) * SIZE_OF_VECTOR);
-        vectorAX = (double*)malloc(sizeof(double) * SIZE_OF_VECTOR);
-        vectorDiffAXB = (double*)malloc(sizeof(double) * SIZE_OF_VECTOR);
-        matrixA = (double*)malloc(sizeof(double) * SIZE_OF_VECTOR * SIZE_OF_VECTOR);
-
-        sizesOfPartsOfVector = (int*)malloc(sizeof(int) * numberOfProcesses);
-        sizesOfPartsOfMatrix = (int*)malloc(sizeof(int) * numberOfProcesses);
-        displacementsOfPartsOfVector = (int*)malloc(sizeof(int) * numberOfProcesses);
-        displacementsOfPartsOfMatrix = (int*)malloc(sizeof(int) * numberOfProcesses);
-
         setDataFirstVariant(matrixA, vectorB, vectorX, vectorX1, vectorAX, vectorDiffAXB);
         fillSizesOfParts(sizesOfPartsOfVector, sizesOfPartsOfMatrix, numberOfProcesses);
         fillDisplacementsOfParts(displacementsOfPartsOfVector, displacementsOfPartsOfMatrix, sizesOfPartsOfVector, sizesOfPartsOfMatrix, numberOfProcesses);
@@ -187,21 +190,20 @@ int main(int argc, char** argv) {
     MPI_Bcast(displacementsOfPartsOfVector, numberOfProcesses, MPI_INT, ZERO_PROCESS, MPI_COMM_WORLD);
     MPI_Bcast(displacementsOfPartsOfMatrix, numberOfProcesses, MPI_INT, ZERO_PROCESS, MPI_COMM_WORLD);
 
-    partOfMatrixA = (double*)malloc(sizeof(double) * sizesOfPartsOfMatrix[currentProcess]);
-    partOfVectorAX = (double*)malloc(sizeof(double) * sizesOfPartsOfVector[currentProcess]);
-    partOfVectorDiffAXB = (double*)malloc(sizeof(double) * sizesOfPartsOfVector[currentProcess]);
-    partOfVectorX = (double*)malloc(sizeof(double) * sizesOfPartsOfVector[currentProcess]);
-    partOfVectorX1 = (double*)malloc(sizeof(double) * sizesOfPartsOfVector[currentProcess]);
+    double* partOfMatrixA = (double*)malloc(sizeof(double) * sizesOfPartsOfMatrix[currentProcess]);
+    double* partOfVectorAX = (double*)malloc(sizeof(double) * sizesOfPartsOfVector[currentProcess]);
+    double* partOfVectorDiffAXB = (double*)malloc(sizeof(double) * sizesOfPartsOfVector[currentProcess]);
+    double* partOfVectorX1 = (double*)malloc(sizeof(double) * sizesOfPartsOfVector[currentProcess]);
 
     setDataToZero(partOfVectorAX, sizesOfPartsOfVector[currentProcess]);
     setDataToZero(partOfVectorDiffAXB, sizesOfPartsOfVector[currentProcess]);
-    setDataToZero(partOfVectorX, sizesOfPartsOfVector[currentProcess]);
     setDataToZero(partOfVectorX1, sizesOfPartsOfVector[currentProcess]);
-    
-    MPI_Scatterv(matrixA, sizesOfPartsOfMatrix, displacementsOfPartsOfMatrix, MPI_DOUBLE, partOfMatrixA, sizesOfPartsOfMatrix[currentProcess], MPI_DOUBLE, ZERO_PROCESS, MPI_COMM_WORLD);
-    MPI_Bcast(vectorB, SIZE_OF_VECTOR, MPI_DOUBLE, ZERO_PROCESS, MPI_COMM_WORLD);
 
-    calculateSquaredNormOfVector(&squaredNormB, vectorB, sizesOfPartsOfVector, currentProcess);
+    calculateSquaredNormOfVector(&squaredNormB, vectorB, sizesOfPartsOfVector, displacementsOfPartsOfVector, currentProcess);
+ 
+    MPI_Scatterv(matrixA, sizesOfPartsOfMatrix, displacementsOfPartsOfMatrix, MPI_DOUBLE, partOfMatrixA, sizesOfPartsOfMatrix[currentProcess], MPI_DOUBLE, ZERO_PROCESS, MPI_COMM_WORLD); 
+    MPI_Bcast(vectorB, SIZE_OF_VECTOR, MPI_DOUBLE, ZERO_PROCESS, MPI_COMM_WORLD);
+    MPI_Bcast(vectorX, SIZE_OF_VECTOR, MPI_DOUBLE, ZERO_PROCESS, MPI_COMM_WORLD);
 
     while(!(isSolved(partOfMatrixA, vectorX, vectorB, partOfVectorAX, partOfVectorDiffAXB, squaredNormB, EPSILON, sizesOfPartsOfVector, displacementsOfPartsOfVector, currentProcess))) {
         getNextX(vectorX, partOfVectorDiffAXB, partOfVectorX1, TAU, sizesOfPartsOfVector, displacementsOfPartsOfVector, currentProcess);
@@ -210,10 +212,10 @@ int main(int argc, char** argv) {
     if (currentProcess == ZERO_PROCESS) {
         end = MPI_Wtime();
         printf("Time spent: %.2lf seconds.\n", end - start);
-        printVector(vectorXn, SIZE_OF_VECTOR);
+        printVector(vectorX, SIZE_OF_VECTOR);
     }
     
-    freeMemory(vectorXn, vectorXn1, vectorB, vectorAXn, vectorDiffAXnB, matrixA);
+    freeMemory(vectorX, vectorX1, vectorB, vectorAX, vectorDiffAXB, matrixA, partOfMatrixA, partOfVectorX1, partOfVectorAX, partOfVectorDiffAXB, sizesOfPartsOfVector, sizesOfPartsOfMatrix, displacementsOfPartsOfVector, displacementsOfPartsOfMatrix);
     MPI_Finalize();
     
     return 0;
